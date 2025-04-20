d9af6ed6098737f3e7108b5e4865390185e62873
Nick Seryakov <nick.seryakov@gmail.com>
2025-04-20 13:53:12 +0400

    Adjust styles

--- lib/components/Fields/ValidationTooltip.tsx
+++ lib/components/Fields/ValidationTooltip.tsx
@@ -4,6 +4,7 @@ import styles from './styles/input.module.scss';
 export const ValidationTooltip = (props: TooltipProps) => (
   <Tooltip
     {...props}
+    data-tooltip-validation
     disabled={!props.content}
     placement="top-start"
     trigger="hover focus click"
