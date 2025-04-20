0577d39cdf7eb2f13604570bb6e7d31c057bb2ca
Nick Seryakov <nick.seryakov@gmail.com>
2025-04-20 13:48:07 +0400

    Icon color schemes

--- lib/components/Icon/Icon.tsx
+++ lib/components/Icon/Icon.tsx
@@ -10,6 +10,7 @@ type SVGComponent = React.FunctionComponent<SVGProps>;
 type Icons = Record<string, SVGComponent>;
 type IconName = (typeof iconNames)[number];
 type Size = string | number;
+type Scheme = 'info' | 'warning' | 'error' | 'success' | 'neutral';
 
 // https://vitejs.dev/guide/features.html#glob-import
 const icons: Icons = import.meta.glob<SVGComponent>('../../assets/icons/*.svg', {
@@ -29,12 +30,13 @@ Object.keys(icons).map((path) => {
 export type IconProps = SVGProps & {
   name?: IconName;
   size?: Size | [Size, Size];
+  scheme?: Scheme;
   color?: string;
   svg?: SVGComponent;
 };
 
 export const Icon = React.forwardRef<SVGSVGElement, IconProps>(
-  ({ name, size, color, svg, style, className, ...props }, ref) => {
+  ({ name, size, scheme, color, svg, style, className, ...props }, ref) => {
     const [width, height] = Array.isArray(size) ? size : [size, size];
     const Svg = svg || (name ? icons[name] : undefined);
 
@@ -45,7 +47,7 @@ export const Icon = React.forwardRef<SVGSVGElement, IconProps>(
         focusable={false}
         aria-hidden={!props['aria-label'] || undefined}
         data-icon={name || Svg.name}
-        className={cn(styles.icon, className)}
+        className={cn(styles.icon, scheme && styles[scheme], className)}
         style={{ ...style, ...cssProps({ color, width, height }) }}
       />
     );
