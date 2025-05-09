e757fb9f81a33faa63378563e5b62b22e254065a
Nick Seryakov <nick.seryakov@gmail.com>
2025-05-10 02:33:34 +0400

    Update Menu component

--- lib/components/Menu/MenuContext.ts
+++ lib/components/Menu/MenuContext.ts
@@ -5,6 +5,7 @@ export type MenuContextProps = {
   active: number | null;
   setOpen: (value: boolean) => void;
   getItemProps: UseInteractionsReturn['getItemProps'];
+  onSelect?: (index: number) => void;
 };
 
 export const MenuContext = React.createContext<MenuContextProps>({} as MenuContextProps);
