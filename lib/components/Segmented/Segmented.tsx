1e90f2c9fe84d3ecc569e7b4310bf851a27deb6b
Nick Seryakov <nick.seryakov@gmail.com>
2025-08-23 00:39:29 +0400

    Utilize ResizeObserver for Segmented indicator position

--- lib/components/Segmented/Segmented.tsx
+++ lib/components/Segmented/Segmented.tsx
@@ -1,4 +1,5 @@
 import cn from 'classnames';
+import { useEffect, useState } from 'react';
 import { Composite, CompositeItem } from '@floating-ui/react';
 
 import styles from './styles.module.scss';
@@ -23,40 +24,64 @@ export const Segmented = ({
   style,
   onSelect,
   ...props
-}: SegmentedProps) => (
-  <Composite
-    {...props}
-    role="radiogroup"
-    data-segmented
-    data-disabled={disabled}
-    className={cn(styles.segmented, className)}
-    style={{
-      ...style,
-      ...cssProps({
-        size,
-        margin,
-        length: items.length.toString(),
-        selected: selected.toString(),
-      }),
-    }}
-  >
-    {items.map((item, index) => (
-      <CompositeItem
-        key={index}
-        render={
-          <button
-            role="radio"
-            type="button"
-            disabled={disabled}
-            data-segmented-item={index}
-            aria-checked={index === selected}
-            data-selected={index === selected}
-            onClick={() => index !== selected && onSelect(index)}
-          >
-            {wrapNode(item, 'span')}
-          </button>
-        }
-      />
-    ))}
-  </Composite>
-);
+}: SegmentedProps) => {
+  const [widths, setWidths] = useState<number[]>([]);
+  const selectedWidth = widths[selected];
+  const selectedOffset = widths.reduce((acc, w, i) => (i < selected ? acc + w : acc), 0);
+
+  useEffect(() => {
+    const resizeObserver = new ResizeObserver((entries) => {
+      const sizes = [];
+
+      for (const entry of entries) {
+        sizes.push(entry.borderBoxSize[0].inlineSize);
+      }
+
+      setWidths(sizes);
+    });
+
+    for (const item of document.querySelectorAll('[data-segmented-item]')) {
+      resizeObserver.observe(item);
+    }
+
+    return () => resizeObserver.disconnect();
+  }, []);
+
+  return (
+    <Composite
+      {...props}
+      role="radiogroup"
+      data-segmented
+      data-disabled={disabled}
+      className={cn(styles.segmented, className)}
+      style={{
+        ...style,
+        ...cssProps({
+          size,
+          margin,
+          width: selectedWidth,
+          offset: selectedOffset,
+        }),
+      }}
+    >
+      {items.map((item, index) => (
+        <CompositeItem
+          key={index}
+          render={
+            <button
+              role="radio"
+              type="button"
+              disabled={disabled}
+              data-segmented-item={index}
+              aria-checked={index === selected}
+              data-selected={index === selected}
+              onClick={() => index !== selected && onSelect(index)}
+            >
+              {wrapNode(item, 'span')}
+            </button>
+          }
+        />
+      ))}
+    </Composite>
+  );
+};
