059e2a3ca7904f0324e00b2aff170fd2c125b4f8
Nick Seryakov <nick.seryakov@gmail.com>
2026-02-02 18:50:18 +0400

    Improve Gallery component tests

--- lib/components/Gallery/Gallery.test.tsx
+++ lib/components/Gallery/Gallery.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
 
 import { Gallery } from './Gallery';
 import styles from './styles.module.scss';
@@ -7,15 +7,25 @@ describe('Gallery', () => {
   const observeMock = vi.fn();
   const unobserveMock = vi.fn();
   const disconnectMock = vi.fn();
+  let entriesCallback: (entries: { target: HTMLElement }[]) => void;
 
   beforeEach(() => {
     class ResizeObserverMock {
+      constructor(callback: (entries: { target: HTMLElement }[]) => void) {
+        entriesCallback = callback;
+      }
       observe = observeMock;
       unobserve = unobserveMock;
       disconnect = disconnectMock;
     }
 
     vi.stubGlobal('ResizeObserver', ResizeObserverMock);
+
+    vi.stubGlobal(
+      'getComputedStyle',
+      // imitates computed style with 3 grid columns
+      vi.fn(() => ({ gridTemplateColumns: '1fr 1fr 1fr' })),
+    );
   });
 
   it('renders with default props and children', () => {
@@ -46,9 +56,17 @@ describe('Gallery', () => {
     const root = screen.getByTestId('root');
     expect(observeMock).toHaveBeenCalledWith(root);
 
+    // simulates resize event for the root element
+    act(() => entriesCallback([{ target: root }]));
+
+    // columns matches the value returned by getComputedStyle mock
+    expect(root.dataset.galleryCols).toBe('3');
+
+    // stops observing when the cols defined
     rerender(<Gallery data-testid="root" cols={5} />);
     expect(unobserveMock).toHaveBeenCalledWith(root);
 
+    // disconnects observer on unmount
     unmount();
     expect(disconnectMock).toHaveBeenCalled();
   });
